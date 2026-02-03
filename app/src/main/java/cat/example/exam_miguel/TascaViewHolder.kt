b96f7d4dfcd79b3add2b2e703733417dba752860
Mikleiper <mimuba55@gmail.com>
2026-02-03 19:26:35 +0100

    1.2

---
+++ app/src/main/java/cat/example/exam_miguel/TascaViewHolder.kt
@@ -0,0 +1,13 @@
+package cat.example.exam_miguel
+
+class TascaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val nom:
+
+}
+
+
+id: Int,
+val nom: String,
+val categoria: Categoria,
+val data: String,
+val estat
